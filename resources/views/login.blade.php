bcbc5e145628f9b8ec4a6e0326493f6615e856bc
kmnii <tjr0916@naver.com>
2018-03-08 10:36:10 +0900

    test
    test

---
+++ resources/views/login.blade.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<body>
+login page<p></p>
+<form name="login">
+    <input type='TEXT' name = 'id'> <!-- 아이디 텍스트-->
+    <p></p>
+    <input type='TEXT' name = 'password'> <!--  비밀번호 텍스트 -->
+    <p></p>
+    <input type='BUTTON' value="login" onclick=""> <!--  로그인 버튼-->
+</form>
+</body>
+</head>
+</html>
