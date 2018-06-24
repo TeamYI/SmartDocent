12a7d909c244af4a92e737f06c6837ff305a01cf
TeamYI <staro_2@naver.com>
2018-06-24 21:18:17 +0900

    20180624

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -23,44 +23,59 @@
 
     <!-- Custom styles for this template -->
     <link href="css/freelancer.min.css" rel="stylesheet">
-
+    <style>
+        .menu-list {
+            width: 280px;
+            height: 180px;
+            right: 85px;
+            position: absolute;
+            padding: 20px;
+            background: #f2eeee;
+        }
+    </style>
 </head>
 
 <body id="page-top">
-
-<!-- Navigation -->
-<nav class="navbar navbar-expand-lg bg-secondary fixed-top text-uppercase" id="mainNav" style="height: 70px; background: #6e5950 !important ;">
-    <div class="container" style="margin : 0 30px; width: 100%">
-        <a class="navbar-brand js-scroll-trigger" href="#page-top">SMART DOCENT</a>
-        <button class="navbar-toggler navbar-toggler-right text-uppercase bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            Menu
-            <i class="fa fa-bars"></i>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto" style="position: absolute; right: 100px;">
-                <li class="nav-item mx-0 mx-lg-1">
-                    <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="/culturalManage">코스 등록</a>
-                </li>
-                <li class="nav-item mx-0 mx-lg-1">
-                    <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="/manage_list">코스 관리</a>
-                </li>
-                <li class="nav-item mx-0 mx-lg-1">
-                    <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="'/member_manage'">회원 관리</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</div>
-
-</nav>
-
 <!-- Header -->
-<section class="masthead bg-primary text-white text-center" style="width:100%; height: 580px;">
-    <img class="img-fluid mb-5 d-block mx-auto" src="image/king.jpg" alt="" style="position: absolute; top:70px;height: 580px; width:100%">
-    <div style="position: absolute; top:70px;width:50%; left:25%; height: 580px; background: white; opacity: 0.7; ;z-index:1" ></div>
-    <div style="position: absolute;top: 50%;left: 50%;width: 600px;height: 100px;margin-top: -200px;margin-left: -290px; font-size: 85px; font-weight: bold ; letter-spacing: 20px; color:#6e5950 !important;z-index: 999">SMART</div>
-    <div style="position: absolute; top: 50%;left: 50%;width: 900px;height: 100px;margin-top: -100px;margin-left: -440px;font-size: 85px; font-weight: bold; letter-spacing: 20px; color:#6e5950 !important;z-index: 999">DOCENT</div>
-    <hr  style="position: absolute; top:65%;left: 57%;width: 550px;height: 10px;margin-top: -100px;margin-left: -370px;border:2px;z-index: 900; background: white">
+<section class="masthead bg-primary text-white text-center" style="width:100%; height: 700px; position: relative; background: white !important;">
+    <img class="img-fluid mb-5 d-block mx-auto" src="image/background.png" alt="" style="position: absolute; top:0;left:270px ; height: 770px; width:1000px; ">
+    <img src="image/line.png" style="position: absolute; top:80px;left:370px; width: 750px; height: 600px;">
+    <img src="image/mainLogo.png" style="position: absolute; top:150px;left:410px; width: 500px;">
+    <img src="image/whiteLine.png" style="position: absolute; top:320px;left:410px; width: 670px;">
+    <span style="color: white; font-size: 25px; font-weight: bold; z-index: 999; position: absolute; top: 350px; left: 410px;">" 사용자 행동 맞춤 지능형 문화재 해설 서비스 "</span>
+    <div class="menu-list" style="top: 80px;">
+        <div style="border: 5px solid #cebeba; padding-top: 5px; position: absolute; width: 180px; height: 140px;">
+            <img src="image/regcourse.png" style="width: 170px; height: 80px;" alt="">
+            <span style="color: #231b19; font-weight: bold ; font-size: 18px; position: absolute; left: 50px; bottom: 10px;">코스 등록</span>
+        </div>
+        <a href="/culturalManage">
+            <div style="width: 60px; height: 180px ;background: #cebeba ; position: absolute;top: 0;right: 0;">
+                <img src="image/right-chevrons.png" style="width: 30px; height: 30px; position: absolute; top: 68px; left: 18px;" alt="">
+            </div>
+        </a>
+    </div>
+    <div class="menu-list" style="top: 290px ;">
+        <div style="border: 5px solid #cebeba; padding-top: 5px; position: absolute; width: 180px; height: 140px;">
+            <img src="image/manageCourse.png" style="width: 170px; height: 80px;" alt="">
+            <span style="color: #231b19; font-weight: bold ; font-size: 18px; position: absolute; left: 50px; bottom: 10px;">코스 관리</span>
+        </div>
+        <a href="/manage_list">
+            <div style="width: 60px; height: 180px ;background: #cebeba ; position: absolute;top: 0;right: 0;">
+                <img src="image/right-chevrons.png" style="width: 30px; height: 30px; position: absolute; top: 68px; left: 18px;" alt="">
+            </div>
+        </a>
+    </div>
+    <div class="menu-list" style="top: 500px ;">
+        <div style="border: 5px solid #cebeba; padding-top: 5px; position: absolute; width: 180px; height: 140px;">
+            <img src="image/userManage.png" style="width: 170px; height: 80px;" alt="">
+            <span style="color: #231b19; font-weight: bold ; font-size: 18px; position: absolute; left: 50px; bottom: 10px;">회원 관리</span>
+        </div>
+        <a href="/member_manage">
+            <div style="width: 60px; height: 180px ;background: #cebeba ; position: absolute;top: 0;right: 0;">
+                <img src="image/right-chevrons.png" style="width: 30px; height: 30px; position: absolute; top: 68px; left: 18px;" alt="">
+            </div>
+        </a>
+    </div>
 </section>
 
 <!-- Bootstrap core JavaScript -->
